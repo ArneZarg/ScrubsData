90450824c1c79161efa2829f88acefb6fa423b89
Arne Zargarian <zargarian.arne@gmail.com>
2020-01-13 16:10:20 -0800

    Create Reply.cs

---
+++ Reply.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example.Models.Domain.Forums
+{
+    public class Reply
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int PostId { get; set; }
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
