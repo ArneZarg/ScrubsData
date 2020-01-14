0ca29b020fa3ac9a4d44093788018f3fead4598a
Arne Zargarian <zargarian.arne@gmail.com>
2020-01-13 16:09:38 -0800

    Create Post.cs

---
+++ Post.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example.Models.Domain.Forums
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int ThreadId { get; set; }
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
